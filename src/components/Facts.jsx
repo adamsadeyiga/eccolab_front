3fc1d5fce57108f6feef5ac568e6001eceb8dd0a
Obadina Fisayo <olufisayobadina@gmail.com>
2023-04-10 15:31:33 +0100

    Another commit

--- src/components/Facts.jsx
+++ src/components/Facts.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FaBookOpen } from 'react-icons/fa'
 
 const Facts = (url) => {
   return (
@@ -6,27 +7,28 @@ const Facts = (url) => {
         <h2 className='title'>Facts About Us</h2>
         <div className="facts-section-container">
             <div className="fact">
-                <img src={url} alt="" />
+                <img src='../../public/img/priscilla-du-preez-ggeZ9oyI-PE-unsplash.jpg' alt="" />
+                <p>Boarding Facilities</p>
+            </div>
+            <div className="fact">
+                <img src='../../public/img/priscilla-du-preez-ggeZ9oyI-PE-unsplash.jpg' alt="" />
+                <p>E-learning Platform</p>
+            </div>
+            <div className="fact">
+                <img src='../../public/img/priscilla-du-preez-ggeZ9oyI-PE-unsplash.jpg' alt="" />
+                <p></p>
+            </div>
+            <div className="fact">
+                {/* <img src='../../public/img/priscilla-du-preez-ggeZ9oyI-PE-unsplash.jpg' alt="" /> */}
+                <FaBookOpen />
+                <p>Functional Library</p>
+            </div>
+            <div className="fact">
+                <img src='../../public/img/priscilla-du-preez-ggeZ9oyI-PE-unsplash.jpg' alt="" />
                 <p>Simple</p>
             </div>
             <div className="fact">
-                <img src={url} alt="" />
-                <p>Simple</p>
-            </div>
-            <div className="fact">
-                <img src={url} alt="" />
-                <p>Simple</p>
-            </div>
-            <div className="fact">
-                <img src={url} alt="" />
-                <p>Simple</p>
-            </div>
-            <div className="fact">
-                <img src={url} alt="" />
-                <p>Simple</p>
-            </div>
-            <div className="fact">
-                <img src={url} alt="" />
+                <img src='../../public/img/priscilla-du-preez-ggeZ9oyI-PE-unsplash.jpg' alt="" />
                 <p>Simple</p>
             </div>
         </div>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -9,7 +9,6 @@ const Main = () => {
             <p className="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae illum hic facere exercitationem ipsum eligendi, odio a blanditiis ab, voluptate ipsa, et animi aspernatur unde eveniet odit doloremque accusamus vero beatae nihil amet veniam!</p>
             <div className="btn-holders">
               <Button text="LogIn" classStatus={true}/>
-              <Button text="SignUp" />
             </div>
         </div>
     </div>
