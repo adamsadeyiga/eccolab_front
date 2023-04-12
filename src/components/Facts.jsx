0e677123423ae0fdd2b2d0f240fb2643604fbe6e
Obadina Fisayo <olufisayobadina@gmail.com>
2023-04-12 17:39:30 +0100

    Another Commit

--- src/components/Facts.jsx
+++ src/components/Facts.jsx
@@ -6,28 +6,31 @@ const Facts = (url) => {
     <div className='facts-section'>
         <h2 className='title'>Facts About Us</h2>
         <div className="facts-section-container">
+            {/* It access img files directly from public folder  */}
+                {/* <img src='/img/hostel.svg' alt="" /> */}
             <div className="fact">
-                <img src='../../public/img/hostel.svg' alt="" />
+
+                <img src='/img/hostel.svg' alt="" />
                 <p>Boarding Facilities</p>
             </div>
             <div className="fact">
-                <img src='../../public/img/e-learning.svg' alt="" />
+                <img src='/img/e-learning.svg' alt="" />
                 <p>E-learning Platform</p>
             </div>
             <div className="fact">
-                <img src='../../public/img/computer labs.svg' alt="" />
+                <img src='/img/computer labs.svg' alt="" />
                 <p>Furnished Computer Labs</p>
             </div>
             <div className="fact">
-                <img src='../../public/img/books library.svg' alt="" />
+                <img src='/img/books library.svg' alt="" />
                 <p>Functional Library</p>
             </div>
             <div className="fact">
-                <img src='../../public/img/sports.svg' alt="" />
+                <img src='/img/sports.svg' alt="" />
                 <p>Extra Curricular Activities</p>
             </div>
             <div className="fact">
-                <img src='../../public/img/excursions.svg' alt="" />
+                <img src='/img/excursions.svg' alt="" />
                 <p>Excursions</p>
             </div>
         </div>
