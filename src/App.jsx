31cad0df0e9df6202c539b406eaa741eacaa4615
Obadina Fisayo <olufisayobadina@gmail.com>
2023-04-15 15:58:10 +0100

    Another Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
 import Header from './components/Header'
 import Main from './components/Main'
 import Facts from './components/Facts'
 import Login from './components/Login'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+// import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 
 const WEBSITE_NAME = 'Eccowas College'
@@ -16,18 +12,18 @@ function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <Router>
-      <div className="main-page">
-        <Header title={WEBSITE_NAME}/>
-        <Route path='/' exact render={(props) => (
+    // <Router>
+    <div className="main-page">
+         <Header title={WEBSITE_NAME}/>
+         {/* <Route path='/' exact render={(props) => ( */}
             <> 
               <Main />  
               <Facts url={imgUrl} /> 
             </>
-          )} /> 
-          <Route path='/login' component={Login} /> 
+          {/* )} />  */}
+          {/* <Route path='/login' component={Login} />  */}
     </div> 
-    </Router>
+    // </Router>
   )
 }
 
